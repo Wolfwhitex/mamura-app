2eedda993b937bbdd1f57083eca8abd16731ff71
kadekadhi120 <i.nugraha005@binus.ac.id>
2024-06-16 21:27:46 +0800

    fix page pesanan pada navbar

--- resources/views/pesanan.blade.php
+++ resources/views/pesanan.blade.php
@@ -12,43 +12,6 @@
       </div>
     </div>
 
-    <!-- header section strats -->
-    <header class="header_section">
-        <div class="container-fluid">
-          <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="{{ url('/cust_page') }}">
-              <span>
-                Mamura
-              </span>
-            </a>
-  
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class=""> </span>
-            </button>
-  
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="{{ url('/cust_page') }}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/pesanan') }}"> Pesanan</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('keranjang') }}">Keranjang</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('histori') }}">Histori</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('/cust_profile') }}"> <i class="fa fa-user" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </header>
-    <!-- end header section -->
   </div>
 
   <!-- about section -->
